34359e55fd53d3a55b0fd096792751c081d0a7ad
meghnagoel10 <goel.meghna10@gmail.com>
2018-02-05 19:29:30 +0530

    First Commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleLightContent();
+    }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
---
+++ js/services.js
@@ -0,0 +1,9 @@
+angular.module('starter.services', [])
+
+.factory('', function() {
+  return {
+    get: function() {    
+      return null;
+    }
+  };
+});
